e5354f1a9e1c53ca4c238a1c6226c9fee4784603
Sarah Laflamme <sarahtheflame@gmail.com>
2015-11-09 11:06:50 -0500

    Modification des modèles thèmes (ajout du cascade delete)

--- modeles_theme.py
+++ modeles_theme.py
@@ -23,24 +23,24 @@ class Style(Base):
 	couleur_fond = Column(String(250))
 	opacite = Column(Integer)
 	bordure_id = Column(Integer, ForeignKey('Bordures.id'),nullable=False)
-	bordure = relationship(Bordure, uselist=False)
+	bordure = relationship(Bordure, uselist=False, cascade='delete,all')
 
 class Theme(Base):
 	__tablename__ = 'Themes'
 	id = Column(Integer, primary_key=True)
 	nom = Column(String(250),nullable=False)
 	titre_id = Column(Integer, ForeignKey('Styles.id'),nullable=False)
-	titre = relationship(Style, uselist=False)
+	titre = relationship(Style, uselist=False, cascade='delete,all')
 	sous_titre_id = Column(Integer, ForeignKey('Styles.id'),nullable=False)
-	sous_titre = relationship(Style, uselist=False)
+	sous_titre = relationship(Style, uselist=False, cascade='delete,all')
 	texte_id = Column(Integer, ForeignKey('Styles.id'),nullable=False)
-	texte = relationship(Style, uselist=False)
+	texte = relationship(Style, uselist=False, cascade='delete,all')
 	tableau_titre_id = Column(Integer, ForeignKey('Styles.id'),nullable=False)
-	tableau_titre = relationship(Style, uselist=False)
+	tableau_titre = relationship(Style, uselist=False, cascade='delete,all')
 	tableau_sous_titre_id = Column(Integer, ForeignKey('Styles.id'),nullable=False)
-	tableau_sous_titre = relationship(Style, uselist=False)
+	tableau_sous_titre = relationship(Style, uselist=False, cascade='delete,all')
 	tableau_texte_id = Column(Integer, ForeignKey('Styles.id'),nullable=False)
-	tableau_texte = relationship(Style, uselist=False)
+	tableau_texte = relationship(Style, uselist=False, cascade='delete,all')
 
 engine = create_engine('sqlite:///src///data/database.db')
 
