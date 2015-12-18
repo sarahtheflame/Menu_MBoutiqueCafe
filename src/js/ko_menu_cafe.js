abd8f8191c384620263bafdf0389412af5ff59cc
IMFUZZ <da.junior.du@gmail.com>
2015-12-17 23:48:23 -0500

    Suppression de librairie js inutile pour l'instant

--- src/js/ko_menu_cafe.js
+++
@@ -1,5 +0,0 @@
-$.getJSON( "data_cafe.json", function( json ) {
-        var data = json;
-        var viewModel = ko.mapping.fromJS(data);
-        ko.applyBindings(viewModel);
-    });
--- src/js/ko_menu_dessert.js
+++
@@ -1,5 +0,0 @@
-$.getJSON( "src/js/data_dessert.json", function( json ) {
-        var data = json;
-        var viewModel = ko.mapping.fromJS(data);
-        ko.applyBindings(viewModel);
-    });
