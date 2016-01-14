b4977ecb2f15dd30cfefbbfc21c34ed789463d99
IMFUZZ <da.junior.du@gmail.com>
2016-01-14 16:06:58 -0500

    ajout des fonctionnalités de 'get' pour le controleur de chaque vue

--- controleurs/accueil.py
+++ controleurs/accueil.py
@@ -15,28 +15,105 @@ s = session()
 def get_affichage(s, nom_fenetre):
     return s.query(Fenetre).filter(Fenetre.nom == nom_fenetre).one().serialiser_en_json()
 
-def get_gestion(s, nom_vue):
-    if nom_vue == "lister_fenetre":
-        get_lister_fenetre(s)
-    elif nom_vue == "medias":
-        get_medias(s)
+def get_gestion(s, data):
+    if data['nom_vue'] == "lister_fenetres":
+        return get_lister_fenetres(s)
+    elif data['nom_vue'] == "medias":
+        return get_medias(s)
+    elif data['nom_vue'] == "themes":
+        return get_lister_themes(s)
+    elif data['nom_vue'] == "parametres":
+        return get_parametres(s)
+    elif data['nom_vue'] == "periodes":
+        return get_lister_periodes(s)
+    elif data['nom_vue'] == "modifier_zone_image":
+        return get_modifier_zone_image(s, data['id'])
+    elif data['nom_vue'] == "modifier_zone_table":
+        return get_modifier_zone_table(s, data['id'])
+    elif data['nom_vue'] == "modifier_fenetre":
+        return get_modifier_fenetre(s, data['id'])
+    elif data['nom_vue'] == "modifier_zone_base":
+        return get_modifier_zone_base(s, data['id'])
+    elif data['nom_vue'] == "modifier_zone_video":
+        return get_modifier_zone_video(s, data['id'])
+    elif data['nom_vue'] == "modifier_theme":
+        return get_modifier_theme(s, data['id'])
 
-def get_lister_fenetre(s):
-    liste_fenetres = {
-        'fenetres' : []
-    }
+def get_lister_fenetres(s):
+    resultats = { 'fenetres' : [] }
     for fenetre in s.query(Fenetre).all():
-        liste_fenetres['fenetres'].append(fenetre.serialiser_en_json())
-    return liste_fenetres
+        resultats['fenetres'].append(fenetre.serialiser_en_json())
+    return resultats
 
 def get_medias(s):
-    liste_medias = {
-        'medias' : []
-    }
+    resultats = { 'medias' : [] }
     for media in s.query(Media).all():
-        liste_medias['medias'].append(media.serialiser_en_json())
-    print(json.dumps(liste_medias, indent=4, separators=(',', ': ')))
-    return liste_medias
+        resultats['medias'].append(media.serialiser_en_json())
+    return resultats
 
-get_gestion(s, "medias")
-# print(json.dumps(data[0], indent=4, separators=(',', ': ')))
+def get_modifier_theme(s, id_theme):
+    resultats = { 
+        'theme' : ''
+    }
+    resultats['theme'] = s.query(Theme).filter(Theme.id == id_theme).one().serialiser_en_json()
+    return resultats
+
+def get_lister_themes(s):
+    resultats = { 'themes' : [] }
+    for theme in s.query(Theme).all():
+        resultats['themes'].append(theme.serialiser_en_json())
+    return resultats
+
+def get_parametres(s):
+    resultats = { 'administrateurs' : [] }
+    for administrateur in s.query(Administrateur).all():
+        resultats['administrateurs'].append(administrateur.serialiser_en_json())
+    return resultats
+
+def get_lister_periodes(s):
+    resultats = { 'periodes' : [] }
+    for periode in s.query(Periode).all():
+        resultats['periodes'].append(periode.serialiser_en_json())
+    return resultats
+
+def get_modifier_zone_image(s, id_zone):
+    resultats = { 
+        'zone_image' : ''
+    }
+    resultats['zone_image'] = s.query(ZoneImage).filter(ZoneImage.id == id_zone).one().serialiser_en_json()
+    return resultats
+
+def get_modifier_zone_video(s, id_zone):
+    resultats = { 
+        'zone_video' : ''
+    }
+    resultats['zone_video'] = s.query(ZoneVideo).filter(ZoneVideo.id == id_zone).one().serialiser_en_json()
+    return resultats
+
+def get_modifier_zone_table(s, id_zone):
+    resultats = { 
+        'zone_table' : ''
+    }
+    resultats['zone_table'] = s.query(ZoneTable).filter(ZoneTable.id == id_zone).one().serialiser_en_json()
+    return resultats
+
+def get_modifier_zone_base(s, id_zone):
+    resultats = { 
+        'zone_base' : ''
+    }
+    resultats['zone_base'] = s.query(ZoneBase).filter(ZoneBase.id == id_zone).one().serialiser_en_json()
+    return resultats
+
+def get_modifier_fenetre(s, id_fenetre):
+    resultats = { 
+        'fenetre' : ''
+    }
+    resultats['fenetre'] = s.query(Fenetre).filter(Fenetre.id == id_fenetre).one().serialiser_en_json()
+    return resultats
+
+data = get_gestion(s, {'nom_vue' : 'modifier_theme', 'id' : '1'})
+print(json.dumps(data, indent=4, separators=(',', ': ')))
+
+# fenetre_repas = s.query(Fenetre).filter(Fenetre.id == 1).one()
+# json_fenetre = fenetre_repas.serialiser_en_json()
+# print(json.dumps(json_fenetre, indent=4, separators=(',', ': ')))
