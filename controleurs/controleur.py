1b89a5b6611df25cfd7623267412e22140f70548
IMFUZZ <da.junior.du@gmail.com>
2016-02-12 08:10:09 -0500

    fix: reload de page quand les modifications sont sauvegardées, modifications de base_gestion.less pour la redimension avec le sidebar

--- controleurs/controleur.py
+++ controleurs/controleur.py
@@ -105,7 +105,7 @@ def get_modifier_zone(s, id_zone):
     return resultats
 
 def get_modifier_fenetre(s, id_fenetre):
-    resultats = { 'fenetre' : '','themes': [], 'zone_focus' : '' ,'vue_associe' : 'modifier_fenetre'  }
+    resultats = { 'fenetre' : '','themes': [], 'vue_associe' : 'modifier_fenetre'  }
     for theme in s.query(Theme).order_by(Theme.id).all():
         resultats['themes'].append(theme.serialiser_en_json())
     resultats['fenetre'] = s.query(Fenetre).filter(Fenetre.id == id_fenetre).one().serialiser_en_json()
