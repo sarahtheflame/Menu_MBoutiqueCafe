f7b9760c50c797410f5474c5d0039c76dbdf3534
Sarah Laflamme <sarahtheflame@gmail.com>
2016-02-15 20:36:56 -0500

    Séparé les scripts js dans des fichiers à part + continué certaines fonctionnalités (ajouter,supprimer)

---
+++ src/js/lister_themes.js
@@ -0,0 +1,25 @@
+$("body").on("click", ".ajouter_theme", function() {
+	var context = ko.contextFor(this);
+	var Theme = function(){
+	  this.id = ko.observable(0);
+	  this.nom = ko.observable($("#nom_theme_input").val());
+	};
+
+	context.$root.themes.push(new Theme());
+	$("#nom_theme_input").val('');
+	$('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+});
+
+$("body").on("click", ".retirer", function() {
+    var context = ko.contextFor(this),
+        id = context.$data.id();
+    if (id != 0) {
+      context.$data.id(-id);
+    } else {
+      context.$parent.themes.remove(context.$data);
+    }
+});
+
+$('body').on('shown.bs.modal', '.modal', function () {
+  $('#nom_theme_input').focus();
+})
