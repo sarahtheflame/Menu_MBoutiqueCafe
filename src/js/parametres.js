fdd4f2340fe6af43895cd5be917030af81208032
IMFUZZ <da.junior.du@gmail.com>
2016-03-01 18:11:28 -0500

    Ajout de hr pour espacer les types de médias, fix: changement du mot de passe de l'administrateur dans la page parametres fonctionnel

--- src/js/parametres.js
+++ src/js/parametres.js
@@ -5,21 +5,19 @@
 $("body").on("click", ".modifier_parametres", function() {
     var nouvelle_adresse_courriel = $('#adresse_courriel_input').val();
     var nouveau_mot_de_passe = $('#mot_de_passe_input').val();
-    if ($('#confirmation_mot_de_passe_input').val() === nouveau_mot_de_passe) {
-        viewModel.administrateur.adresse_courriel(nouvelle_adresse_courriel);
-        viewModel.administrateur.mot_de_passe(nouveau_mot_de_passe);
-    } 
+    // if ($('#confirmation_mot_de_passe_input').val() === nouveau_mot_de_passe) {
+    //     viewModel.administrateur.adresse_courriel(nouvelle_adresse_courriel);
+    //     viewModel.administrateur.mot_de_passe(nouveau_mot_de_passe);
+    // } 
+    viewModel.administrateur.adresse_courriel(nouvelle_adresse_courriel);
+    viewModel.administrateur.mot_de_passe(nouveau_mot_de_passe);
 });
 
 /**
  * Affiche ou cache le mot de passe
  * Lancé par un clic sur un élément portant la classe toggle_password_2
  */
-$("body").on("click", ".toggle_password_2", function() {
-    if ($('#mot_de_passe_input').attr('type') == 'password') {
-        $('#mot_de_passe_input').attr('type', 'text');
-    }
-    else {
-        $('#mot_de_passe_input').attr('type', 'password');
-    }
+$("body").on("click", "#toggle_mot_de_passe", function() {
+    $('#confirmation_mot_de_passe_input').get(0).type = 'password';
+    $('#mot_de_passe_input').get(0).type = 'password';
 });
